845acd07bfe4b14ce1f3afe0787c418b5813649d
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-14 19:08:36 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,13 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(filepath)
+    @path = filepath
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+  end
+  
+end
