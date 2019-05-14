9c6f6cc5366414c8a5f8c725a4e39dcd520e23dc
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-14 19:12:32 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,10 +11,7 @@ class MP3Importer
   end
 
   def import
-    self.files.each do |file|
-      song = Song.new_by_filename(file)
-      Artist.all << song.artist unless Artist.all.include?(song.artist)
-    end
+    self.files
+    @file_array.each{|song| Song.new_by_filename(song)}
   end
-
 end
