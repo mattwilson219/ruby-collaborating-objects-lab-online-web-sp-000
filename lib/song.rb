6171f6283640ed1b6e50e662f68ce09bffb29388
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-14 19:04:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,14 @@ class Song
     @name = name
   end
 
+  def self.new_by_filename(filename)
+    split_name = filename.split(" - ")
+    song = self.new(split_name[1]) 
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
+    song
+  end
+
 
 
 end
